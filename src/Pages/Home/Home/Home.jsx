d99ae005c920deb603ecdb2d10397a73351a326e
sharmin akter <sharmin.happy.25@gmail.com>
2023-06-07 01:23:22 +0600

    created footer

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../Shared/Footer/Footer";
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome in  home page</h2>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Home;
