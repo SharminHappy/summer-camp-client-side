f954681d32e3e034cb4669d6f6cf1a1eeb3a402a
sharmin akter <sharmin.happy.25@gmail.com>
2023-06-07 06:22:10 +0600

    created login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,37 @@
+import { useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../../providers/AuthProvider";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Login = () => {
 
-    const handleLogin=event=>{
+    const { signIn } = useContext(AuthContext);
+    const [passwordShown, setPasswordShown] = useState(false);
+
+
+
+    const handleLogin = event => {
         event.preventDefault();
-        const form=event.target;
-        const email=form.email.value;
-        const password=form.password.value;
-        console.log(email,password)
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+    }
+
+    const togglePassword = () => {
+        if (passwordShown == 'password') {
+            setPasswordShown(!passwordShown)
+            return
+
+        }
+        setPasswordShown('password')
+
     }
     return (
         <div>
@@ -18,7 +41,7 @@ const Login = () => {
             <div className="hero min-h-screen bg-base-200 ">
                 <div className="hero-content  w-full ">
                     <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-                        <form  onSubmit={handleLogin} className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -29,10 +52,20 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <input type={passwordShown ? "text" : "password"} name="password" placeholder="password" className="input input-bordered"></input>
+                                <div className="input-group-btn mx-auto">
+                                    <button  onClick={togglePassword}>
+                                    {
+                                        passwordShown == 'password' ? <FaEye className=" absolute -translate-y-12 translate-x-32"></FaEye> :
+                                            <FaEyeSlash className=" absolute -translate-y-12 translate-x-32"></FaEyeSlash>
+
+                                    }
+
+                                </button></div>
+
                             </div>
                             <div className="form-control mt-6">
-                                
+
                                 <input className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
