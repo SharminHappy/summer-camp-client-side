79e9cfd7a34c486256e9ae95d135b5274eb6fcb2
sharmin akter <sharmin.happy.25@gmail.com>
2023-06-08 21:50:34 +0600

    created a useSelect custom hook by using tanStack query

---
+++ src/hooks/useSelect.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+
+const useSelect = () => {
+    const { user } = useContext(AuthContext);
+
+    const {refetch, data: select = [] } = useQuery({
+        queryKey: ['selects', user?.email],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:5000/selects?email=${user?.email}`)
+            return response.json();
+        },
+    })
+    return [select,refetch]
+
+};
+
+export default useSelect;
